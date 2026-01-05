c7b062a66332713b2c37d157fc1a34e245756640
jaojaofu <huynhphugiao02@gmail.com>
2026-01-05 11:00:48 +0700

    feat: Configure Vite for production and PWA
    Updates Vite configuration to optimize for production builds and enable PWA features. This includes updating the asset path, enabling directory emptying, and adjusting the Vercel configuration for better deployment. Also, refactors the service worker and manifest for PWA compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './index.html'
       }
     }
-  },
-  publicDir: false // Chúng ta sẽ để Vite tự xử lý vì các file đang ở root
+  }
 });
